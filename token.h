76902dc90b3a4fad4d63e139eafc7f7e755e919b
keithmuller <66692457+keithmuller@users.noreply.github.com>
2022-10-12 14:51:02 -0700

    Set 2 as the min number of columns

--- token.h
+++ token.h
@@ -6,7 +6,7 @@
 /*
  * Constants (some are for testing only)
  */
-#define MINCOL  1       /* min number of columns */
+#define MINCOL  2       /* min number of columns */
 #define MAXCOL  50      /* max number of columns */
 #define BUFSZ   8192    /* input buffer size limit */
 #define INDELIM ','     /* input column delimiter */
